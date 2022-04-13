889f7b56e89365697cc8beb29ba67809ce56fa27
Galiheee <galianbase@gmail.com>
2022-04-13 23:12:42 +0700

    Commit message

--- app/src/main/java/com/example/highandlowgame/MainActivity.java
+++ app/src/main/java/com/example/highandlowgame/MainActivity.java
@@ -11,35 +11,39 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     int randomNumber;
-    public void randomNumberGenerator()
-    {
+
+    public void randomNumberGenerator() {
         Random rand = new Random();
-        randomNumber = rand.nextInt(100)+1;
+        randomNumber = rand.nextInt(100) + 1;
     }
 
-    public void onGuess(View view)
-    {
-        EditText editTextNumber = (EditText) findViewById(R.id.editTextNumber);
-        int GuessedNumber = Integer.parseInt(editTextNumber.getText().toString());
+    public void onGuess(View view) {
         String message;
-        if (GuessedNumber < randomNumber)
-            message = "Higher !!";
-        else if (GuessedNumber > randomNumber)
-            message = "Lower !!";
-        else
-        {
-            message = "You Got Me !!";
-            randomNumberGenerator();
+        try {
+            EditText editTextNumber = (EditText) findViewById(R.id.editTextNumber);
+            int GuessedNumber = Integer.parseInt(editTextNumber.getText().toString());
+            if (GuessedNumber < 1 || GuessedNumber > 100)
+                message = "Input a number between 1-20!";
+            else if (GuessedNumber < randomNumber)
+                message = "Higher !!";
+            else if (GuessedNumber > randomNumber)
+                message = "Lower !!";
+            else {
+                message = "You Got Me !!";
+                randomNumberGenerator();
+            }
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            message = "Input Number !!";
         }
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
-    }
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        }
+        @Override
+        protected void onCreate (Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
 
-        Random rand = new Random();
-        randomNumber = rand.nextInt(100+1);
-
+            Random rand = new Random();
+            randomNumber = rand.nextInt(100) + 1;
+        }
     }
-}
